d91044911e8ed5e1dc74cf6b773508cf88990b1f
Justyna Malinowska <justynamalinowska758@gmail.com>
2024-11-10 00:17:22 +0100

    Update components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
+  let items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <ListGroup />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>
+          This is an Alert!
+        </Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>"Click me!"</Button>
     </div>
   );
 }
